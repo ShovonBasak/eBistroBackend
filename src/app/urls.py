1314ec8ed4a0b57e915691e3cc65da644b35531f
Shovon Basak <shvnbasak@gmail.com>
2023-09-22 21:19:16 +0600

    Garbage URL is removed

--- src/app/urls.py
+++ src/app/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', view.hello_world, name='hello_world'),
 ]
